349912897e3b2b833e17d5a29fd5262ea9352227
Anas-123Baig <165074024+Anas-123Baig@users.noreply.github.com>
2024-03-27 01:18:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "Please select one operator",
+        name: "operator",
+        type: "list",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    }
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else
+    console.log("Invalid input");
---
+++ main.ts
@@ -0,0 +1,31 @@
+
+  import inquirer from "inquirer";
+  import prompt from "inquirer/lib/ui/prompt.js";
+  let answer=await  inquirer.prompt([
+      {message:"Enter your first number",type:"number",name:"firstNumber"},
+     { message:"Enter your second number",type:"number",name:"secondNumber"},
+    {
+      message:"Please select one operator",
+      name:"operator",
+      type:"list",
+      choices:["addition","subtraction","multiplication","division"]
+    }
+  ])
+  if(answer.operator==="addition"){
+      console.log(answer.firstNumber + answer.secondNumber)
+     
+  }
+  else if(answer.operator==="subtraction"){
+      console.log(answer.firstNumber - answer.secondNumber)
+  }
+  else if(answer.operator==="multiplication"){
+      console.log(answer.firstNumber * answer.secondNumber)
+  }
+  else if(answer.operator==="division"){
+      console.log(answer.firstNumber / answer.secondNumber)
+  }
+  else console.log("Invalid input")
+  
+
+
+ 
