c0f7aec808cb1931192efbc8f1935caef41441c7
Anas-123Baig <165074024+Anas-123Baig@users.noreply.github.com>
2024-05-12 19:46:27 +0500

    Add files via upload
    Assignment 3

---
+++ Namecase.js
@@ -0,0 +1,6 @@
+// NameFormat.ts
+let name = "Eric";
+console.log(name.toLowerCase()); // prints the name in lowercase
+console.log(name.toUpperCase()); // prints the name in uppercase
+console.log(name.charAt(0).toUpperCase() + name.slice(1)); // prints the name in titlecase
+export {};
---
+++ Namecase.ts
@@ -0,0 +1,7 @@
+// NameFormat.ts
+
+let name: string = "Eric";
+
+console.log(name.toLowerCase()); // prints the name in lowercase
+console.log(name.toUpperCase()); // prints the name in uppercase
+console.log(name.charAt(0).toUpperCase() + name.slice(1)); // prints the name in titlecase
