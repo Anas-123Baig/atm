cb7736a9c7304b578c64d91bea97e087a27bbaaf
Anas-123Baig <165074024+Anas-123Baig@users.noreply.github.com>
2024-05-12 19:41:11 +0500

    Add files via upload
    Assignment No 2

---
+++ PersonalGreeting.js
@@ -0,0 +1,6 @@
+// PersonalGreeting.ts
+// Store the person's name in a variable
+let personName = "Eric";
+// Print a message to that person
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
+export {};
---
+++ PersonalGreeting.ts
@@ -0,0 +1,7 @@
+// PersonalGreeting.ts
+
+// Store the person's name in a variable
+let personName: string = "Eric";
+
+// Print a message to that person
+console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
